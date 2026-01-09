534b97e8a0eb0186f50c09438fb14bd23d3a2d82
28davidrusnac-dotcom <28.david.rusnac@carolinaday.org>
2026-01-09 16:52:47 +0000

    Code

---
+++ hi.py
@@ -0,0 +1,31 @@
+a=float(input("enter first number"))
+b=float(input("enter second number"))
+
+print("""
+Choose an operation:
++ addition
+- Substraction
+* Multiplication
+/ Division
+""")
+
+op= input("operation:")
+
+if op=="+":
+    result=a+b
+
+elif op== "-":
+    result= a-b
+elif op== "*":
+    result= a*b
+elif op== "/":
+    if b==0:
+        print("Division by zero is not allowed.")
+        exit()
+    result= a/b
+
+else:
+    print("invalid operation.")
+
+
+print(f"result: {result}")
